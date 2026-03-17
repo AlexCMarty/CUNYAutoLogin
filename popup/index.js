e31220921ae6c6f4a5fb0b1cfab9838a50c25638
Alexander Marty <alex@alexmarty.dev>
2026-03-16 21:16:51 -0400

    feat: made html and css for username, email, totp entry

---
+++ popup/index.js
@@ -0,0 +1,17 @@
+/*<form id="myForm">
+  <input type="text" id="username" placeholder="Enter name" />
+  <input type="number" id="age" placeholder="Enter age" />
+  <button type="submit">Submit</button>
+</form>
+
+<script>
+  document.getElementById('myForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // stops any page reload or network request
+
+    const username = document.getElementById('username').value;
+    const age = document.getElementById('age').value;
+
+    console.log('Got:', username, age);
+    // do whatever you want with the data here
+  });
+</script>*/
