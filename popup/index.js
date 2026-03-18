027a5363e4093f4e172057fd50f6b181a2247e7b
Alexander Marty <alex@alexmarty.dev>
2026-03-18 17:53:37 -0400

    feat: added input validation and submit handling for info entry

--- popup/index.js
+++ popup/index.js
@@ -1,17 +1,41 @@
-/*<form id="myForm">
-  <input type="text" id="username" placeholder="Enter name" />
-  <input type="number" id="age" placeholder="Enter age" />
-  <button type="submit">Submit</button>
-</form>
+function getFormData() { // Either returns object or error
+  let data = Object.fromEntries(new FormData(document.getElementById('info-form')));
+  data['email'] = data['email'].trim();
+  data['totp-code'] = data['totp-code'].trim();
+  const re = /@login.cuny.edu$/;
+  
 
-<script>
-  document.getElementById('myForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // stops any page reload or network request
+  if (Object.values(data).some((val) => !val)) {
+    return {result: 'error', message: 'All fields are required'};
+  }
 
-    const username = document.getElementById('username').value;
-    const age = document.getElementById('age').value;
+  if (!document.getElementById('email').validity.typeMismatch) {
+    return {result: 'error', message: 'Doesn\'t look like a valid email.'};
+  }
 
-    console.log('Got:', username, age);
-    // do whatever you want with the data here
-  });
-</script>*/
+  if (!re.test(data['email'])) {
+    return {result: 'error', message: 'Must enter a CUNY email, ending in @login.cuny.edu'};
+  }
+
+  if (data['password-initial'] !== data['password-confirmation']) {
+    return {result: 'error', message: 'Passwords do not match'};
+  }
+
+  data.result = 'result';
+  return data;
+}
+
+function showError(errorMessage) {
+  console.log(errorMessage);
+}
+
+document.getElementById('submit-button').addEventListener('click', () => {
+  const data = getFormData();
+  if (data.result == 'result') { // Oh how I long for Rust's enums! Result<T, E>!!!
+    console.log(data);
+  } else {
+    showError(data.message);
+  }
+});
+
+console.log('entered');
